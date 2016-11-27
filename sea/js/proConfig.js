ee6d9e37ee6c96b6ee68e4a308ef6f47eb2ac9e4
Bob Ke <961904564@qq.com>
2016-11-27 19:20:13 +0800

    Update proConfig.js

--- sea/js/proConfig.js
+++ sea/js/proConfig.js
@@ -1,6 +1,9 @@
-define(function() {
-	return {
-		a:123,
-		b:456
-	}
-})
+define(function(require, exports, module) {
+	
+	var obj = {
+		aaa:1,
+		bbb:2
+	};
+
+	module.exports = obj;// 导出模块，效果跟return obj是一样的。
+});
